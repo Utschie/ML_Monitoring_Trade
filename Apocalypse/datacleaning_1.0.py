240e0520f4a823af1634ee5751b0490dee9100b3
Utschie <littlecat.j@hotmail.com>
2020-06-18 17:29:09 +0200

    20200618

--- Apocalypse/datacleaning_1.0.py
+++ Apocalypse/datacleaning_1.0.py
@@ -2,16 +2,19 @@
 #每个urlnum代表一场单独的比赛，同样的urlnum下，不同的cid是同一个公司对该厂比赛的不同赔率，再同一个cid下不同的restime是不同的剩余时间
 #用sql很麻烦，改用pandas做成数据框，然后直接洗试试
 #现在是把字典列表逐个插入dataframe，写进一天的文件要好多分钟————20200618
+#一个76.5M的原文件写入dataframe要5472秒，压缩成csv变成28.6M，读取只需1秒，所以要先把所有的文件转成csv文件————20200618
 import re
 import datetime
 import pandas as pd
-import ast
+import time
 #从文本文件读入记录，由于当时写入文件的时候没分行也没分分隔符，所以整个文件只有一行
 f=open('D:\\data\\20141130-20160630\\2014-11-30.txt','r')
 line=f.readline()
+f.close()#要关闭数据集
 #把这一行数据用re转成列表
 datalist=re.findall('{.*?}',line)
 #把列表中各个元素转成字典,并且把peilv，gailv和kailizhishu分拆成三列，否则无法正确读入pandas
+start=time.time()
 for i in range(0,len(datalist)-1):
     datalist[i]=eval(datalist[i])#先转成字典
     del datalist[i]['timestamp']#去掉时间戳
@@ -35,8 +38,10 @@ for i in range(0,len(datalist)-1):
         df=df.append(datalist[i],ignore_index=True)
     print(str(i))#显示添加进度
 
+end=time.time()
+print('耗时'+str(end-start)+'秒')
 
-
+df.to_csv('D:\\data_csv\\2014-11-30.csv',index=False)#写入csv文件,且不写入右边的序号
 
 
 #把列表中各个字典去除时间戳后逐个插入mysql
