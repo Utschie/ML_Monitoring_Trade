c720d6c7fe9cd48ec9f6025f946139ee0a8285fd
Utschie <littlecat.j@hotmail.com>
2020-06-21 06:50:52 +0200

    20200621

--- Apocalypse/datacleaning_1.0.py
+++ Apocalypse/datacleaning_1.0.py
@@ -4,8 +4,10 @@
 #应该想办法把数据缩小，比如看能不能用多维数组之类的
 #尝试用xarray然后以netCDF格式（.nc）存储，但是出了个问题，float object has no attribute 'encode'
 #可能是因为数据集里有缺失值nan被当做字符串了，但是后面又有浮点数据，所以出错。
-#只要设一个frametime作为索引，然后把所有的表连起来就可以了
+#只要设一个frametime作为索引，然后把所有的表连起来就可以了，这样就可以存在一个csv里————20200621
+#当前的速度是一天比赛的数据预处理需要668秒，但是数据大小是2.3G，下一步是改用netCDF格式，以及去除一些无用的部分数据，看是否速度提升体积下降————20200621
 #下一步是可能搞一个双索引（urlnum,frametime）,然后一天的比赛都存在一个dataframe里————20200621
+
 from gevent import monkey;monkey.patch_all()
 import gevent
 import re
@@ -13,6 +15,7 @@ import datetime
 import pandas as pd
 import time
 import csv
+start=time.time()
 '''先转成csv'''
 #进入文件夹，生成文件名序列
 f=open('D:\\data\\20141130-20160630\\2014-11-30.txt','r')
@@ -59,7 +62,7 @@ urlnumlist=list(df['urlnum'].value_counts().index)#获得比赛列表
 bisailist=list(map(bisaiquery,urlnumlist))#获得由各个比赛的dataframe组成的表
 
 
-def bisai2netCDF(bisai):#把单场比赛转换成netCDF文件
+def bisai2csv(bisai):#把单场比赛转换成csv文件
     urlnum=str(bisai.urlnum.values[0])
     resttimelist=list(bisai.resttime.value_counts().sort_index(ascending=False).index)#获得该场比赛的变盘列表并排序
     dfdict=list()
@@ -69,15 +72,19 @@ def bisai2netCDF(bisai):#把单场比赛转换成netCDF文件
         df=df.set_index('frametime')
         dfdict.append(df.drop_duplicates('cid',keep='last'))
     newdict=pd.concat(dfdict)#一个新的
+    newdict.to_csv('D:\\data_csv\\'+urlnum+'.csv')
+
     
     
 def coprocess(bisailist):#用协程的方式并发写入
     ge = list()
     for i in bisailist:
-        ge.append(gevent.spawn(bisai2netCDF,i))
+        ge.append(gevent.spawn(bisai2csv,i))
     gevent.joinall(ge)
 
 coprocess(bisailist)
+end=time.time()
+print('耗时'+str(end-start)+'秒')
 
 
 
