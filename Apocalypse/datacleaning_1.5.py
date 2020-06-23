6ad7f003be50493ed25efe26ddb05f0d7527af7b
Utschie <littlecat.j@hotmail.com>
2020-06-24 01:18:34 +0200

    20200624

--- Apocalypse/datacleaning_1.5.py
+++ Apocalypse/datacleaning_1.5.py
@@ -1,6 +1,7 @@
 #此文件是datacleaning_1.5，比1.0提升部分性能
 #删除了冗余数据，只保留与最简单的模型相关的数据后，时间变成470秒，数据体积767MB————20200621
 #最原始的txt数据大小是76.5M,转换后变成767M，数据体积扩大了10倍。还算可以接受的范围
+#暂时还没有测试————20200624
 from gevent import monkey;monkey.patch_all()
 import gevent
 import re
@@ -8,18 +9,28 @@ import datetime
 import pandas as pd
 import time
 import csv
-start=time.time()
+import os#用与建立文件夹
 '''先转成csv'''
 #进入文件夹，生成文件名序列
-f=open('D:\\data\\20141130-20160630\\2014-11-30.txt','r')
-line=f.readline()
-f.close()#要关闭数据集
-#把这一行数据用re转成列表
-datalist=re.findall('{.*?}',line)
-keys=['date', 'urlnum', 'cid', 'resttime', 'fanhuanlv', 
-'peilv_host', 'peilv_fair', 'peilv_guest', 
-'kailizhishu_host', 'kailizhishu_fair', 'kailizhishu_guest']
-df=pd.DataFrame(columns=keys)#先列好字段
+def txt2csv(date):
+    filepath='D:\\data\\20141130-20160630\\'+date+'.txt'#讲日期转成文件名
+    f=open(filepath,'r')
+    line=f.readline()
+    f.close()#要关闭数据集
+    #把这一行数据用re转成列表
+    datalist=re.findall('{.*?}',line)
+    keys=['date', 'urlnum', 'cid', 'resttime', 'fanhuanlv', 
+    'peilv_host', 'peilv_fair', 'peilv_guest', 
+    'kailizhishu_host', 'kailizhishu_fair', 'kailizhishu_guest']
+    df=pd.DataFrame(columns=keys)#先列好字段
+    dictlist=list(map(str2dict,datalist))#把datalist变成了字典形式的dictlist，即每个元素都是字典
+    with open('F:\\data_csv\\'+date+'.csv','w',newline='') as f:#
+        w=csv.DictWriter(f,keys)
+        w.writeheader()
+        w.writerows(dictlist)
+    df=pd.read_csv('F:\\data_csv\\'+date+'.csv',encoding='GBK')#在移动硬盘内写入同名的csv文件并读取
+    return df
+
 #把列表中各个元素转成字典,并且把peilv，gailv和kailizhishu分拆成三列，否则无法正确读入pandas
 def str2dict(str):#讲datalist中的单个元素转换插入dataframe的函数
     dic=eval(str)
@@ -40,21 +51,13 @@ def str2dict(str):#讲datalist中的单个元素转换插入dataframe的函数
     return dic
 
 
-dictlist=list(map(str2dict,datalist))#把datalist变成了字典形式的dictlist，即每个元素都是字典
-with open('D:\\data_csv\\2014-11-30.csv','w',newline='') as f:
-    w=csv.DictWriter(f,keys)
-    w.writeheader()
-    w.writerows(dictlist)
-'''再拆成单个比赛，写入文件夹'''
-df=pd.read_csv('D:\\data_csv\\2014-11-30.csv',encoding='GBK')
 def bisaiquery(num):
     bisai=df.loc[lambda dfa:dfa.urlnum==num]
     return bisai
 
-urlnumlist=list(df['urlnum'].value_counts().index)#获得比赛列表
-bisailist=list(map(bisaiquery,urlnumlist))#获得由各个比赛的dataframe组成的表
 def bisai2csv(bisai):#把单场比赛转换成csv文件
     urlnum=str(bisai.urlnum.values[0])
+    date=str(bisai.date.values[0])
     resttimelist=list(bisai.resttime.value_counts().sort_index(ascending=False).index)#获得该场比赛的变盘列表并排序
     dfdict=list()
     for i in resttimelist:
@@ -64,7 +67,8 @@ def bisai2csv(bisai):#把单场比赛转换成csv文件
         dfdict.append(df.drop_duplicates('cid',keep='last'))
     newdict=pd.concat(dfdict)#一个新的
     newdict=newdict.drop(columns=['resttime','urlnum','date'])
-    newdict.to_csv('D:\\data_csv\\'+urlnum+'.csv')
+    outputpath='F:\\cleaned_data_20141130-20160630\\'+date+'\\'+urlnum+'.csv'
+    newdict.to_csv(outputpath)
 
     
     
@@ -74,7 +78,19 @@ def coprocess(bisailist):#用协程的方式并发写入
         ge.append(gevent.spawn(bisai2csv,i))
     gevent.joinall(ge)
 
-coprocess(bisailist)
-end=time.time()
-print('耗时'+str(end-start)+'秒')
+
+filelist = os.listdir('D:\\data\\20141130-20160630')#读出这一年半的数据文件名
+datelist=[i[0:-4] for i in filelist]
+for i in datelist:
+    outputpath='F:\\cleaned_data_20141130-20160630\\'+i#为每一天建立一个文件夹
+    os.makedirs(outputpath)#建立文件夹
+    df=txt2csv(i)#讲txt文件导出csv后读入dataframe
+    urlnumlist=list(df['urlnum'].value_counts().index)#获得当天比赛列表
+    bisailist=list(map(bisaiquery,urlnumlist))#获得由各个比赛的dataframe组成的表
+    coprocess(bisailist)#协程并发分
+
+
+
+
+
 
