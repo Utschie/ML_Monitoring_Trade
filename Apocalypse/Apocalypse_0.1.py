a93bbec5f5900d88fe72631cd32b529fc95873ce
Utschie <littlecat.j@hotmail.com>
2020-07-24 00:16:05 +0200

    20200724

--- Apocalypse/Apocalypse_0.1.py
+++ Apocalypse/Apocalypse_0.1.py
@@ -9,17 +9,19 @@
 #上述情况或者是把其他未更新数据用东西填充，或者是把公司数*指标数这样一个当前状态的矩阵转化成一个固定大小的东西，比如“密度棒”
 #0.1版本先用0元素填充方式，不管用什么填充，反正没信息的那个节点不能被激活
 #把公司id按从小到大排列然后组成一个公司数*指标数的矩阵，保证传入的每个矩阵的对应行都对应相同的公司
+#另外给定的环境模型除了赔率矩阵外，还要有终止状态，一个就是钱花完了的状态，还有一个就是终赔出来的状态
+#还是应该先数据预处理，处理成一个水位的n_company*1的向量，然后根据得出的q值解码出怎么买，来减小策略空间的数量————20200724
 
 
 #先写一个神经网络类
 import tensorflow as tf
-class Dataloader():#需要定义一个数据预处理器，把每次不同大小的状态矩阵转成固定大小
+class Dataloader():#需要定义一个数据预处理器，把每次的状态矩阵转化成一个向量
     def __init__(self):
         #传入原始数据，为一个不定长张量对象
         print('数据预处理器初始化完成')
         
 
-    def load(self,origin:tf.RaggedTensor):
+    def load(self,origin):
         self.origin=origin
 
 
@@ -42,4 +44,8 @@ class Q_Model(tf.keras.Model):
         self.n_features = n_features
         self.n_actions = n_actions
         super().__init__()#调用tf.keras.Model的类初始化方法
+        self.flatten = tf.keras.layers.Flatten() #把单个矩阵展平
+        self.dense1 = tf.keras.layers.Dense(units=100, activation=tf.nn.relu)#第一个全连接层
+        self.dense2 = tf.keras.layers.Dense(units=self.n_companies*3)#暂时是觉得有这么多种策略，但还是觉得怪怪的
+        
         
