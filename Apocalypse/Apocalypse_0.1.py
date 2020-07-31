a0397e6f63b08e75cd73c33bcb9f8f15b74a2451
Utschie <littlecat.j@hotmail.com>
2020-07-31 09:51:12 +0200

    20200731

--- Apocalypse/Apocalypse_0.1.py
+++ Apocalypse/Apocalypse_0.1.py
@@ -41,6 +41,8 @@
 #或许不需要中间结算，只要capital小于0了，那之后的一律无法行动，这样的话，capital也可以作为一个状态变量传入神经网络————20200731
 #最后出可视化的时候应该把算一下收益率————20200731
 #环境的结算方法可能存在一些问题————20200731
+#只在终盘时结算，其他时候收益都是负值，因为如果提前跳出会导致浪费样本————20200731
+#算revenue时给超出资金量的策略的收益赋予很大的负值，然后把capital也放在变量里送给神经网络(已解决)
 
 '''
 两种可能：第一种是把矩阵数据预处理成一个向量，然后输出一个向量再解码成策略
@@ -73,13 +75,12 @@ class Env():#定义一个环境用来与网络交互
         self.cidlist = list(map(float,cidlist))#把各个元素字符串类型转成浮点数类型
         self.filepath = filepath
         self.episode = self.episode_generator(self.filepath)#通过load_toNet函数得到当场比赛的episode生成器对象
-        self.capital = 100#每场比赛有500欧可支配资金
+        self.capital = 500#每场比赛有500欧可支配资金
         self.invested = [[(0.,0.),(0.,0.),(0.,0.)]]#已投入资本，每个元素记录着一次投资的赔率和投入，分别对应胜平负三种赛果的投入，这里不用np.array因为麻烦
         self.statematrix = np.zeros((1,410,9))#初始化状态矩阵
         #传入原始数据，为一个不定长张量对象
         print('环境初始化完成')
-        
-
+     
     def episode_generator(self,filepath):#传入单场比赛文件路径，得到一个每一幕的generator
         data = pd.read_csv(filepath)#读取文件
         frametimelist=data.frametime.value_counts().sort_index(ascending=False).index#将frametime的值读取成列表
@@ -112,21 +113,13 @@ class Env():#定义一个环境用来与网络交互
                 revenue = sum(i[1][0]*i[1][1] for i in self.invested )
             else:#主负
                 revenue = sum(i[2][0]*i[2][1] for i in self.invested )
-        else:#如果没到终盘，则收益为负值
+        elif self.capital < sum(action):#如果没到终盘，且action的总投资比所剩资本还多
+            revenue = -100#则收益是个很大的负值（正常来讲revenue最大-50）
+        else:
             revenue = -sum(action)
-            self.capital += revenue#同时从剩余资金里扣
-            if self.capital <= 0:#如果扣完之后没有钱了，则执行清算
-                if self.result.host > self.result.guest:#主胜
-                    revenue += sum(i[0][0]*i[0][1] for i in self.invested )
-                elif self.result.host == self.result.guest:#平
-                    revenue += sum(i[1][0]*i[1][1] for i in self.invested )
-                else:#主负
-                    revenue += sum(i[2][0]*i[2][1] for i in self.invested )
+            self.capital += revenue#该局游戏的capital随着操作减少
         return revenue
-
-
-
-        
+       
     def get_state(self):
         try:
             next_state=self.episode.__next__()
@@ -134,9 +127,8 @@ class Env():#定义一个环境用来与网络交互
         except:
             next_state = np.zeros((1,410,9))
             done = True
-        return next_state, done#网络从此取出下一幕
-
-
+        return next_state, done,self.capital#网络从此取出下一幕
+    
 
         
 
@@ -160,16 +152,15 @@ class Q_Network(tf.keras.Model):
         self.dense2 = tf.keras.layers.Dense(units=int(0.75*self.n_companies*self.n_features), activation=tf.nn.relu)#一个隐藏层
         self.dense3 = tf.keras.layers.Dense(units=self.n_actions)#输出层代表着在当前最大赔率前，买和不买的六种行动的价值
 
-    def call(self,state): #输入从env那里获得的statematrix
-        self.state = state
-        x = self.flatten(self.state)#输出[2100,1]
+    def call(self,state,capital): #输入从env那里获得的statematrix
+        x = np.append(self.flatten(state),[capital])#输出[2100,1]
         x = self.dense1(x)#输出[6300,1]
         x = self.dense2(x)#输出= [4725,1]
         q_value = self.dense3(x)#输出= [6,1]
         return q_value#q_value是一个410*3的矩阵
 
-    def predict(self, state):#用来对应动作
-        q_values = self(state)
+    def predict(self, state,capital):#用来对应动作
+        q_values = self(state,capital)
         return tf.argmax(q_values)#tf.argmax函数是返回最大数值的下标，用来对应动作
 
       
@@ -189,43 +180,38 @@ if __name__ == "__main__":
 
 
     filepath = 'D:\\data\\2014-11-30\\702655.csv'#文件路径
-    bisai_id = int(re.findall('\\\\(\d*?).csv',filepath)[0])#从filepath中得到bisai代码的整型数
+    bisai_id = int(re.findall(r'\\(\d*?).csv',filepath)[0])#从filepath中得到bisai代码的整型数
     result = resultlist.loc[bisai_id]#其中result.host即为主队进球，result.guest则为客队进球
     epsilon = initial_epsilon#得有一个当前epsilon的计算公式
     bianpan_env = Env(filepath,result)#每场比赛做一个环境
     eval_Q = Q_Network()#初始化行动Q网络
     target_Q = Q_Network()#初始化目标Q网络
     replay_buffer = deque(maxlen=10000)#建立一个记忆回放区
-    state =  bianpan_env.get_state()#把第一个状态作为初始化状态
-    done = False
+    state,done,capital=  bianpan_env.get_state()#把第一个状态作为初始化状态
     opt = tf.keras.optimizers.RMSprop(learning_rate)#设定最优化方法
     step_counter = 0
     while True:
-        q_eval = eval_Q.predict(state)#获得行动q_value
+        action_index = eval_Q.predict(state,capital)[0]#获得行动q_value
         if random.random() < epsilon:#如果落在随机区域
             action = random.choice(range(0,1331))#action是一个坐标
         else:
-            action =eval_Q.predict(eval_Q)[0]#否则按着贪心选，这里[0]是因为predict返回的是一个单元素列表
-        revenue = bianpan_env.revenue(actions_table[eval_Q.predict(eval_Q)[0]])#根据行动和是否终赔计算收益
-        next_state, done = bianpan_env.get_state()#获得下一个状态,终止状态的next_state为0矩阵
-        q_target = target_Q(next_state)
+            action = action_index#否则按着贪心选，这里[0]是因为predict返回的是一个单元素列表
+        revenue = bianpan_env.revenue(actions_table[action])#根据行动和是否终赔计算收益
+        next_state,done,next_capital = bianpan_env.get_state()#获得下一个状态,终止状态的next_state为0矩阵
         #这里需要标识一下终止状态，钱花光了就终止了
         if done:#如果终盘了，跳出
-            replay_buffer.append((state, action, revenue, next_state,1))
+            replay_buffer.append((state, action, revenue, next_state,capital,next_capital,1))
             break
         else:#如果没终盘
-            if bianpan_env.capital <=0:#如果钱超过500欧或者变盘结束了，则跳出
-                replay_buffer.append((state, action, revenue, next_state,0))
-                break
-            else:
-                replay_buffer.append((state, action, revenue, next_state,0))
+            replay_buffer.append((state, action, revenue, next_state,capital,next_capital,0))
         state = next_state
+        capital = next_capital
         #下面是参数更新过程
         if len(replay_buffer) >= batch_size:
-            batch_state, batch_action, batch_revenue, batch_next_state ,batch_done= zip(*random.sample(replay_buffer, batch_size))#zip(*...)解开分给别人的意思
-            y_true = batch_revenue+tf.reduce_max(target_Q.predict(batch_next_state),axis = 1)*(1-batch_done)#reduce_max来返回最大值，暂不考虑折现率gamma,
+            batch_state, batch_action, batch_revenue, batch_next_state ,batch_capital,batch_done,batch_next_capital= zip(*random.sample(replay_buffer, batch_size))#zip(*...)解开分给别人的意思
+            y_true = batch_revenue+tf.reduce_max(target_Q.predict(batch_next_state,batch_next_capital),axis = 1)*(1-batch_done)#reduce_max来返回最大值，暂不考虑折现率gamma,
             #tensorflow中张量相乘是对应行相乘，所以eval_Q(batch_state)有多少列，one_hot就得有多少列，如下
-            y_pred = tf.reduce_sum(eval_Q(batch_state)*tf.one_hot(batch_action,depth=1331,on_value=1.0, off_value=0.0),axis=1)#one_hot来生成对应位置为1的矩阵，depth是列数，reduce_sum(axis=1)来求各行和转成一维张量
+            y_pred = tf.reduce_sum(eval_Q(batch_state,batch_capital)*tf.one_hot(batch_action,depth=1331,on_value=1.0, off_value=0.0),axis=1)#one_hot来生成对应位置为1的矩阵，depth是列数，reduce_sum(axis=1)来求各行和转成一维张量
             loss =  tf.keras.losses.mean_squared_error(y_true = y_true,y_pred = y_pred)#y_true和y_pred都是第0维为batch_size的张量
             opt.minimize(loss,eval_Q.variables,grad_loss=None, name=None)
 
