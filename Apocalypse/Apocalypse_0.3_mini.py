132b637fde0b33fc36c8eca9660c6107e865f27e
Utschie <littlecat.j@hotmail.com>
2020-08-13 02:12:54 +0200

    20200813

--- Apocalypse/Apocalypse_0.3_mini.py
+++ Apocalypse/Apocalypse_0.3_mini.py
@@ -9,7 +9,7 @@
 #另外可以考虑一下错误行动的负收益，此处为-100，因为-500时loss降得太快了
 #当-500时差不多到十万次转移可能就可以了，所以这次尝试快速结束随机策略，或者降到很低————202020813
 '''
-即时收益+终赔不参与投资+错误行动收益为0+标准化
+即时收益+终赔不参与投资+错误行动收益为0+标准化+10万次转移转贪心+-100负收益+gamma(0.9)+Adam(0.001)
 '''
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"#这个是使在tensorflow-gpu环境下只使用cpu
@@ -153,9 +153,9 @@ if __name__ == "__main__":
     start0 = time.time()
     summary_writer = tf.summary.create_file_writer('./tensorboard_0.3_mini') #在代码所在文件夹同目录下创建tensorboard文件夹（本代码在jupyternotbook里跑，所以在jupyternotebook里可以看到）
     #########设置超参数
-    learning_rate = 0.0001#学习率
+    learning_rate = 0.001#学习率
     opt = tf.keras.optimizers.Adam(learning_rate)#设定最优化方法
-    gamma = 0.8
+    gamma = 0.9
     epsilon = 1.            # 探索起始时的探索率
     #final_epsilon = 0.01            # 探索终止时的探索率
     batch_size = 500
