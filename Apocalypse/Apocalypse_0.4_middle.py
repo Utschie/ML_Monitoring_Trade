f53ab0cc60b85359ed67634cd7d41a839a9c8ce0
Utschie <littlecat.j@hotmail.com>
2020-08-13 02:23:26 +0200

    20200813

--- Apocalypse/Apocalypse_0.4_middle.py
+++ Apocalypse/Apocalypse_0.4_middle.py
@@ -1,6 +1,6 @@
 '''
 分位数输入+即时最小返还率增量+终赔不参与投资+错误行动-1.0+非标准化+Nadam(0.001)+gamma(0.9)+20万次转移转贪心
-28万7千个参数
+两个隐藏层，24万5千个参数
 '''
 import tensorflow as tf
 from collections import deque
@@ -121,16 +121,12 @@ class Q_Network(tf.keras.Model):
         self.dense1 = tf.keras.layers.Dense(units=144, activation=tf.nn.relu)#输入层
         self.dense2 = tf.keras.layers.Dense(units=144, activation=tf.nn.relu)#一个隐藏层
         self.dense3 = tf.keras.layers.Dense(units=144, activation=tf.nn.relu)
-        self.dense4 = tf.keras.layers.Dense(units=144, activation=tf.nn.relu)
-        self.dense5 = tf.keras.layers.Dense(units=144, activation=tf.nn.relu)
         self.dense6 = tf.keras.layers.Dense(units=self.n_actions)#输出层代表着在当前最大赔率前，买和不买的六种行动的价值
 
     def call(self,state): #输入从env那里获得的statematrix
         x = self.dense1(state)#输出神经网络
         x = self.dense2(x)#
         x = self.dense3(x)
-        x = self.dense4(x)
-        x = self.dense5(x)
         q_value = self.dense6(x)#
         return q_value#q_value是一个（1,1331）的张量
 
