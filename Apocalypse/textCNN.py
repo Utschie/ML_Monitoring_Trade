e049290de62ee4a7dbb35231ddfd9dbd02f548e9
Utschie <littlecat.j@hotmail.com>
2020-11-05 21:46:11 +0100

    20201105

--- Apocalypse/textCNN.py
+++ Apocalypse/textCNN.py
@@ -1,1 +1,7 @@
-#本模型是利用textCNN来做的一个特征提取模型
+#本模型是利用textCNN来做的一个特征提取模型，用的anaconda的tensorflow环境下的pytorch1.5
+import os
+import torch
+from torch import nn
+import torch.utils.data as Data
+import  torch.nn.functional as F
+import sys
