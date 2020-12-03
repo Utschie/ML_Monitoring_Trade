dae53cdf90ec2696afcc0e73140403cba19e4aec
Utschie <littlecat.j@hotmail.com>
2020-12-03 12:43:06 +0100

    20201203

--- Apocalypse/tiqucid.py
+++ Apocalypse/tiqucid.py
@@ -1,8 +1,10 @@
 #寻找cid
 #经过修改后，用来提取20141130-20190224之间全部数据中的cid————20201128
+#不过这个程序比较慢，要遍历10万多个文件
 import pandas as pd
 import os
 cidlist=list()
+filenum = 0#已遍历文件数
 filelist = os.listdir('F:\\cleaned_data_new_20141130-20160630')#读出洗好的数据的文件夹名
 for i in filelist:
     bisailist = os.listdir('F:\\cleaned_data_new_20141130-20160630\\'+i)
@@ -11,6 +13,8 @@ for i in filelist:
         data = pd.read_csv(filepath)
         cid = data.cid.drop_duplicates().values.tolist()
         cidlist = list(set(cidlist+cid))
+        filenum+=1
+        print('已遍历'+str(filenum)+'个文件')
 
 filelist = os.listdir('H:\\cleaned_data_new_20160701-20190224')
 for i in filelist:
@@ -20,6 +24,8 @@ for i in filelist:
         data = pd.read_csv(filepath)
         cid = data.cid.drop_duplicates().values.tolist()
         cidlist = list(set(cidlist+cid))
+        filenum+=1
+        print('已遍历'+str(filenum)+'个文件')
 
 filepath = 'H:\\cidlist_complete.csv'
 ser=pd.Series(cidlist)#转成series
