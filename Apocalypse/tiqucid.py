87a65f1770c8855206a8b642e5c1b89203e26ded
Utschie <littlecat.j@hotmail.com>
2020-12-03 09:40:00 +0100

    20201203

--- Apocalypse/tiqucid.py
+++ Apocalypse/tiqucid.py
@@ -3,20 +3,20 @@
 import pandas as pd
 import os
 cidlist=list()
-filelist = os.listdir('F:\\cleaned_data_20141130-20160630')#读出洗好的数据的文件夹名
+filelist = os.listdir('F:\\cleaned_data_new_20141130-20160630')#读出洗好的数据的文件夹名
 for i in filelist:
-    bisailist = os.listdir('F:\\cleaned_data_20141130-20160630\\'+i)
+    bisailist = os.listdir('F:\\cleaned_data_new_20141130-20160630\\'+i)
     for j in bisailist:
-        filepath = 'F:\\cleaned_data_20141130-20160630\\'+i+'\\'+j
+        filepath = 'F:\\cleaned_data_new_20141130-20160630\\'+i+'\\'+j
         data = pd.read_csv(filepath)
         cid = data.cid.drop_duplicates().values.tolist()
         cidlist = list(set(cidlist+cid))
 
 filelist = os.listdir('H:\\cleaned_data_new_20160701-20190224')
 for i in filelist:
-    bisailist = os.listdir('H:\\cleaned_data_20160701-20190224\\'+i)
+    bisailist = os.listdir('H:\\cleaned_data_new_20160701-20190224\\'+i)
     for j in bisailist:
-        filepath = 'H:\\cleaned_data_20160701-20190224\\'+i+'\\'+j
+        filepath = 'H:\\cleaned_data_new_20160701-20190224\\'+i+'\\'+j
         data = pd.read_csv(filepath)
         cid = data.cid.drop_duplicates().values.tolist()
         cidlist = list(set(cidlist+cid))
