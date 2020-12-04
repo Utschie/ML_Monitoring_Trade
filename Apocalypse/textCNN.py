68e52dca24be6fdba836c6dae3a4e3ad6f2a61ea
Utschie <littlecat.j@hotmail.com>
2020-12-04 09:54:37 +0100

    20201204

--- Apocalypse/textCNN.py
+++ Apocalypse/textCNN.py
@@ -9,6 +9,7 @@
 #本程序用的是20141130-20160630的37897场比赛做训练集，用20160701-20190224做验证集和测试集————20201203
 #最新的cidlist共有600个cid，其中有一些奇怪的3000开头的cid，还没想好要不要去掉————20201204
 #cidlist_complete是全部的cid文件，cid_publice是前后半段时间都共有的cid共有306个————20201204
+#暂时用public的公共cid，因为如果用全部cid可能需要打乱全部10万个训练集的次序，就很麻烦，倒不如用大家都有的一直活着的公司数据————20201204
 import os
 import torch
 from torch import nn
@@ -19,7 +20,7 @@ import pandas as pd
 import numpy as np
 import csv
 
-with open('D:\\data\\cidlist_public.csv') as f:#从cidlist_complete文件里读出306个cid来
+with open('D:\\data\\cidlist_public.csv') as f:
     reader = csv.reader(f)
     cidlist = [row[1] for row in reader]#得到cid对应表
 cidlist = list(map(float,cidlist))#把各个元素字符串类型转成浮点数类型
