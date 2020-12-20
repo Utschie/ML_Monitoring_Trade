eac053a611b23522b542398e64041e04f3cdb61b
Utschie <littlecat.j@hotmail.com>
2020-12-20 23:19:34 +0100

    20201220

---
+++ Apocalypse/text_RNN_half.py
@@ -0,0 +1,148 @@
+#本模型是把序列长度减少一半的textRNN模型，seq_len=250
+#这样参数就变成了52.7万，128的batch，显存1.9G，内存10G，均速约0.2s/场————20201220
+import os
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+import sys
+import pandas as pd
+import numpy as np
+import csv
+import random
+import re
+from sklearn.decomposition import TruncatedSVD
+from torch.nn.utils.rnn import pad_sequence#用来填充序列
+import time
+from prefetch_generator import BackgroundGenerator
+with open('C:\\data\\cidlist_complete.csv') as f:
+    reader = csv.reader(f)
+    cidlist = [row[1] for row in reader]#得到cid对应表
+cidlist = list(map(float,cidlist))#把各个元素字符串类型转成浮点数类型
+class DataLoaderX(DataLoader):
+    def __iter__(self):
+        return BackgroundGenerator(super().__iter__())
+class BisaiDataset(Dataset):#数据预处理器
+    def __init__(self,filepath):
+        self.lablelist = pd.read_csv('C:\\data\\lablelist.csv',index_col = 0)#比赛id及其对应赛果的列表
+        self.filelist0 = [i+'\\'+k for i,j,k in os.walk(filepath) for k in k]#得到所有csv文件的路径列表
+        self.filelist = [data_path for data_path in self.filelist0 if int(re.findall(r'\\(\d*?).csv',data_path)[0]) in  self.lablelist.index]#只保留有赛果的文件路径
+        self.lables = {'win':0,'lose':1,'draw':2}#分类问题要从0开始编号，否则出错
+    
+    def __getitem__(self, index):
+        # TODO
+        # 1. Read one data from file (e.g. using numpy.fromfile, PIL.Image.open).
+        #这里需要注意的是，第一步：read one data，是一个dat
+        data_path = self.filelist[index]
+        bisai_id = int(re.findall(r'\\(\d*?).csv',data_path)[0])
+        # 2. Preprocess the data (e.g. torchvision.Transform).
+        data = self.csv2frame(data_path)
+        # 3. Return a data pair (e.g. image and label).
+        lable = self.lablelist.loc[bisai_id].result
+        lable = self.lables[lable]
+        return data,lable      
+       
+    def __len__(self):
+        # You should change 0 to the total size of your dataset.
+        return len(self.filelist)
+
+    def csv2frame(self,filepath):#给出单场比赛的csv文件路径，并转化成帧列表和对应变帧时间列表，以及比赛结果
+        data = pd.read_csv(filepath)#读取文件
+        data = data.drop(columns=['league','zhudui','kedui','companyname'])#去除非数字的列
+        frametimelist=data.frametime.value_counts().sort_index(ascending=False).index#将frametime的值读取成列表
+        framelist =list()#framelist为一个空列表,长度与frametimelist相同,一定要规定好具体形状和float类型，否则dataloader无法读取
+        '''
+        此处两个循环算法太慢，用pandas更慢，完全抛弃pandas后，数据处理速度从109秒降到了10秒，降到10秒后cpu利用率20%，再往上提也提不上去了，可能需要C++或C来写了
+        '''
+        new_data = np.array(data)
+        lables = new_data[:,0]
+        if len(frametimelist)>250:
+            frametimelist = [frametimelist[0]]+random.sample(list(frametimelist)[1:-1],248)+[frametimelist[-1]]#如果长度大于500,保留头尾，并在中间随机抽取498个，共计500个
+            frametimelist.sort(reverse=True)#并降序排列
+        for i in frametimelist:
+            state = new_data[lables==i]#从第一次变盘开始得到当次转移
+            #state = np.array(state)#不必转成numpy多维数组，因为已经是了
+            state = np.delete(state,(0,1), axis=-1)#去掉frametime和cid
+            #在填充成矩阵之前需要知道所有数据中到底有多少个cid
+            framelist.append(state)
+        frametimelist = np.array(frametimelist)
+        vectensor = self.mrx2vec(framelist)
+        len_frame = vectensor.shape[0]
+        if len_frame<250:
+            vectensor = np.concatenate((np.zeros((250-len_frame,10),dtype=np.float64),vectensor),axis=0)#如果不足500，则在前面用0填充
+        vectensor = torch.from_numpy(vectensor)
+        return vectensor#传出一个帧列表,也可以把frametimelist一并传出来，此处暂不考虑位置参数的问题
+    
+    def tsvd(self,frame):
+        tsvd = TruncatedSVD(1)
+        if frame.shape[0] != 1:
+            newframe = tsvd.fit_transform(np.transpose(frame))#降维成（1,10）的矩阵
+        else:
+            return frame.reshape((10,1))#第一行需要reshape一下
+        return newframe
+    
+    def mrx2vec(self,flist):#把截断奇异值的方法把矩阵变成向量(matrix2vec/img2vec)，传入：len(frametimelist)*(306*10),传出：len(frametimelist)*10
+        vectensor = np.array(list(map(self.tsvd,flist))).squeeze(2)
+        #veclist = veclist.transpose()
+        #vectensor = torch.from_numpy(veclist)#转成张量
+        return vectensor#传出一个形状为(1,序列长度,10)的张量，因为后面传入模型之前，还需要做一下pad_sequence(0维是batch_size维)
+
+ 
+
+class Lstm(nn.Module):#把CNN的结果输入LSTM里
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.LSTM(input_size=10, 
+                                hidden_size=250,#选择对帧进行保留首尾的均匀截断采样
+                                num_layers=1,#暂时就只有一层
+                                bidirectional=True)
+        self.decoder = nn.Linear(1000, 3)#把LSTM的输出
+
+    def forward(self,inputs):
+        output, _= self.encoder(inputs.permute(1,0,2))#inputs需要转置一下再输入lstm层，因为pytorch要求第0维为长度，第二维才是batch_size
+        encoding = torch.cat((output[0], output[-1]), -1)#双向的lstm，就把两个都放进去
+        return self.decoder(encoding)#把最后一个时间步的输出输入MLP
+
+
+def get_parameter_number(model):#参数统计
+    total_num = sum(p.numel() for p in model.parameters())
+    trainable_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return {'Total': total_num, 'Trainable': trainable_num}
+
+
+
+if __name__ == "__main__":
+    root_path = 'C:\\data\\developing'
+    dataset = BisaiDataset(root_path)
+    print('数据集读取完成')
+    loader = DataLoaderX(dataset, 128, shuffle=True,num_workers=8,pin_memory=True)#num_workers>0情况下无法在交互模式下运行
+    print('dataloader准备完成')
+    net = Lstm().double().cuda()#双精度
+    print('网络构建完成')
+    stat1 = get_parameter_number(net)
+    print(str(stat1))
+    lr, num_epochs = 0.001, 2000
+    optimizer= torch.optim.Adam(net.parameters(), lr=lr)
+    loss = nn.CrossEntropyLoss()
+    for epoch in range(1, num_epochs + 1):
+        counter = 0
+        start = time.time()
+        for x, y in iter(loader):
+            #但是还需要使填充后的那些0不参与计算，所以可能需要制作掩码矩阵
+            #或者需要时序全局最大池化层来消除填充的后果
+            x = x.double().cuda()
+            y = y.long().cuda()
+            output = net(x)#x要求是一个固定shape的第0维是batch_size的张量，所以需要批量填充
+            l = loss(output, y)
+            optimizer.zero_grad() # 梯度清零，等价于net.zero_grad()
+            l.backward()
+            optimizer.step()
+            end = time.time()
+            train_period = end-start
+            counter+=1
+            print('第'+str(epoch)+'个epoch已学习'+str(counter)+'个batch,'+'用时'+str(train_period)+'秒')
+            #print('loss: %f' % (l.item()))
+            start = time.time()
+            #torch.cuda.empty_cache()#释放一下显存
+        print('epoch %d, loss: %f' % (epoch, l.item()))
+
